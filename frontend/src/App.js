91033b94e9d242fd3fba5827e74cdd76847b9e69
ImThienz <imthienz@gmail.com>
2024-11-08 23:36:49 +0700

    New commit (video 3)

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-danger'>E-Commerce web in reactjs</h1>
+    </>
+  );
+}
+
+export default App;
